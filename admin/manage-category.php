9ce3b8e116db8f7ff3df03807a8493a9ef8a6129
AmaliaRamadhaniAmandaSyafii <72840325+AmaliaRamadhaniAmandaSyafii@users.noreply.github.com>
2021-10-03 22:26:36 +0700

    Add files via upload

---
+++ admin/manage-category.php
@@ -0,0 +1,56 @@
+<?php include('partials/menu.php'); ?>
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Manage Category</h1>
+
+        <br /><br />
+
+                <!-- Button to Add Admin -->
+                <a href="a" class="btn-primary">Add Category</a>
+
+                <br /><br /><br />
+
+                <table class="tbl-full">
+                    <tr>
+                        <th>S.N.</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                    </tr>
+
+                    <tr>
+                        <td>1. </td>
+                        <td>Amalia Ramadhani</td>
+                        <td>amaliaramadhani</td>
+                        <td>
+                            <a href="a" class="btn-secondary">Update Admin</a>
+                            <a href="a" class="btn-danger">Delete Admin</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>2. </td>
+                        <td>Yogi Firmansyah</td>
+                        <td>yogifirmansyah</td>
+                        <td>
+                            <a href="a" class="btn-secondary">Update Admin</a>
+                            <a href="a" class="btn-danger">Delete Admin</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>3. </td>
+                        <td>Nurma Sari</td>
+                        <td>nurmasari</td>
+                        <td>
+                            <a href="a" class="btn-secondary">Update Admin</a>
+                            <a href="a" class="btn-danger">Delete Admin</a>
+                        </td>
+                    </tr>
+                </table>
+    </div>
+
+</div>
+
+<?php include('partials/footer.php'); ?>
