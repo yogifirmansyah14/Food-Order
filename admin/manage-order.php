f7633ca6ee50fa86ee74a0ebf1b69162c776dd6d
yogifirmansyah14 <91444265+yogifirmansyah14@users.noreply.github.com>
2021-10-18 11:11:07 +0700

    Update manage-order.php

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -1,51 +1,48 @@
 <?php include('partials/menu.php'); ?>
 
-<div class="main-content">
-    <div class="wrapper">
-        <h1>Manage Order</h1>
-              
-                <br /><br /><br />
+    <!-- Main Content Section Starts -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Manage Order</h1>
+            <br><br>
 
-                <table class="tbl-full">
-                    <tr>
-                        <th>S.N.</th>
-                        <th>Full Name</th>
-                        <th>Username</th>
-                        <th>Actions</th>
-                    </tr>
-
-                    <tr>
-                        <td>1. </td>
-                        <td>Amalia Ramadhani</td>
-                        <td>amaliaramadhani</td>
-                        <td>
-                            <a href="a" class="btn-secondary">Update Admin</a>
-                            <a href="a" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>2. </td>
-                        <td>Yogi Firmansyah</td>
-                        <td>yogifirmansyah</td>
-                        <td>
-                            <a href="a" class="btn-secondary">Update Admin</a>
-                            <a href="a" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>3. </td>
-                        <td>Nurma Sari</td>
-                        <td>nurmasari</td>
-                        <td>
-                            <a href="a" class="btn-secondary">Update Admin</a>
-                            <a href="a" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
-                </table> 
+            <table class="tbl-full">
+                <tr>
+                    <th>S.No.</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+                <tr>
+                    <td>1.</td>
+                    <td>Mayank Agarwal</td>
+                    <td>mayank0123</td>
+                    <td>
+                        <a href="#" class=btn-secondary>Update Admin</a>
+                        <a href="#" class=btn-danger>Delete Admin</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2.</td>
+                    <td>Mayank Agarwal</td>
+                    <td>mayank0123</td>
+                    <td>
+                        <a href="#" class=btn-secondary>Update Admin</a>
+                        <a href="#" class=btn-danger>Delete Admin</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>3.</td>
+                    <td>Mayank Agarwal</td>
+                    <td>mayank0123</td>
+                    <td>
+                        <a href="#" class=btn-secondary>Update Admin</a>
+                        <a href="#" class=btn-danger>Delete Admin</a>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
-
-</div>
+    <!-- Main Content Section Ends -->
 
 <?php include('partials/footer.php'); ?>
