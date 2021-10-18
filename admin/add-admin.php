baf68586be8a0607ff6a8a18d293d78442bae8f1
yogifirmansyah14 <91444265+yogifirmansyah14@users.noreply.github.com>
2021-10-18 11:07:51 +0700

    Update add-admin.php

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -1,105 +1,94 @@
 <?php include('partials/menu.php'); ?>
 
-<div class="main-content">
-    <div class="wrapper">
-        <h1>Add Admin</h1>
+    <!-- Main Content Section Starts -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Add Admin</h1>
+            <br><br>
 
-        <br><br>
+            <?php
+                if(isset($_SESSION['add'])){
+                    echo $_SESSION['add'];  // Dispalying session message
+                    unset($_SESSION['add']); // Unset the session
+                }
+            ?>
 
-        <?php
-            if(isset($_SESSION['add']))
-            {
-                echo $_SESSION['add']; //Display the Session Message if SEt
-                unset($_SESSION['add']); //Remove Session Message
-            }
-        ?>
+            <br><br>
 
-        <form action="" method="POST">
+            <form action="" method="POST">
+                <table class="tbl-30">
+                    <tr>
+                        <td>Full Name:</td>
+                        <td>
+                            <input type="text" name="full_name" placeholder="Enter your Name">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Username: </td>
+                        <td>
+                            <input type="text" name="username" placeholder="Enter your User Name">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Password: </td>
+                        <td>
+                            <input type="password" name="password" placeholder="Enter your Password">
+                        </td>
+                    </tr>
 
-            <table class="tbl-30">
-                <tr>
-                    <td>Full Name: </td>
-                    <td>
-                        <input type="text" name="full_name" placeholder="Enter Your Name"></td>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>Username: </td>
-                    <td>
-                        <input type="text" name="username" placeholder="Your Username">
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>Password: </td>
-                    <td>
-                        <input type="password" name="password" placeholder="Your Password">
-                    </td>
-                </tr>
-
-                <tr>
-                    <td colspan="2">
-                        <input type="submit" name="submit" value="Add Admin" class="btn-secondary">
-                    </td>
-                </tr>
-
-            <table>
-
-        </form>
-
-
+                    <tr>
+                        <td colspan="2">
+                            <input type="submit" name="submit" value="Add Admin" class="btn-secondary">
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
     </div>
-</div>
+    <!-- Main Content Section Ends -->
 
-<?php include('partials/footer.php'); ?>
 
+<?php include('partials/footer.php'); ?>  
 
 <?php
-    //Process the Value from Form and Save it in Database
+    // Process the value from form and save it in database
 
-    //Check whether the submit button is clicked or not
-
-    if(isset($_POST['submit']))
-    {
-        // Button Clicked
-        //echo "Button Clicked";
-
-        //1. Get the Data from form
+    // Check the submit button is clicked or not
+    if(isset($_POST['submit'])){
+        // Button clicked
+        
+        // get the data from the form
         $full_name = $_POST['full_name'];
         $username = $_POST['username'];
-        $password = md5($_POST['password']); //Password Encryption with MD5
+        $password = md5($_POST['password']); // Password Encryption with MD5
 
-        //2. SQL Query to Save the data into database
-        $sql = "INSERT INTO tbl_admin SET 
-            full_name='$full_name',
-            username='$username',
-            password='$password',
+        // SQL query to save the data into the database
+
+        $sql = "INSERT INTO tbl_admin SET
+            full_name = '$full_name',
+            username = '$username',
+            password = '$password'
         ";
 
-        //3. Executing Query and Saving Data into Database
-        $res = mysqli_query($conn, $sql) or die(mysqli_error());
+        // Executing the Query and saving data in database
+        $res = mysqli_query($conn,$sql) or die(mysqli_error());
 
-        //4. Check whether the (Query is Executed) data is inserted or not and display appropriate message
-        if ($res==TRUE)
-        {
-            //Data Inserted
-            //echo "Data Inserted";
-            //Create a Session Variable to Display Message
-            $_SESSION['add'] = "Admin Added Successfully";
-            //Redirect Page to Manage Admin
+        // Check whether the data is inserted or not and display appropriate
+        if($res==true){
+            // data inserted
+            // Create a session variable to display message
+            $_SESSION['add'] = "<div class='success'>Admin added successfully</div>";
+            // redirect page to manage admin
             header("location:".SITEURL.'admin/manage-admin.php');
         }
-        else
-        {
-            //FAiled to Insert DAta
-            //echo "Faile to Insert Data";
-            //Create a Session Variable to Display Message
-            $_SESSION['add'] = "Failed to Add Admin";
-            //Redirect Page to Add Admin
+        else{
+            // data not inserted
+            // Create a session variable to display message
+            $_SESSION['add'] = "<div class='error'>Failed to add admin</div>";
+            // redirect page to manage admin
             header("location:".SITEURL.'admin/add-admin.php');
         }
 
     }
-    
+
 ?>
