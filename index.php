5f07440a12697c4296f640291077b928b976a35a
yogifirmansyah14 <91444265+yogifirmansyah14@users.noreply.github.com>
2021-10-31 20:31:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,13 @@
     </section>
     <!-- Food Search Section Ends Here -->
 
+    <?php
+        if(isset($_SESSION['order'])){
+            echo $_SESSION['order'];
+            unset ($_SESSION['order']);
+        }
+    ?>
+
     <!-- Categories Section Starts Here -->
     <section class="categories">
         <div class="container">
@@ -106,7 +113,7 @@
                                         </p>
                                         <br>
 
-                                        <a href="order.php" class="btn btn-primary">Order Now</a>
+                                        <a href="<?php echo SITEURL; ?>order.php?food_id=<?php echo $id; ?>" class="btn btn-primary">Order Now</a>
                                     </div>
                                 </div>
 
