d8b10860a7381ebc2266ceff218000e87bfd9a42
yogifirmansyah14 <91444265+yogifirmansyah14@users.noreply.github.com>
2021-10-31 20:28:31 +0700

    Update categories.php

--- categories.php
+++ categories.php
@@ -17,7 +17,7 @@
                         $title = $row['title'];
                         $image_name = $row['image_name'];
                         ?>
-                            <a href="category-foods.html">
+                            <a href="<?php echo SITEURL; ?>category-foods.php?category_id=<?php echo $id; ?>">
                                 <div class="box-3 float-container">
                                     <?php 
 
