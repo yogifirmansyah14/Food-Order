2d05b970ce8bbe7859b7347adfe78d587845bbe4
nurmasarilarasajii <72746534+nurmasarilarasajii@users.noreply.github.com>
2021-10-31 23:17:09 +0700

    Add files via upload

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -5,41 +5,93 @@
         <div class="wrapper">
             <h1>Manage Order</h1>
             <br><br>
-
+            
+            <?php
+            if(isset($_SESSION['update'])) {
+                echo $_SESSION['update'];
+                unset ($_SESSION['update']);
+            }
+            ?>
             <table class="tbl-full">
                 <tr>
-                    <th>S.No.</th>
-                    <th>Full Name</th>
-                    <th>Username</th>
+                    <th>S.N.</th>
+                    <th>Food</th>
+                    <th>Price</th>
+                    <th>Qty.</th>
+                    <th>Total</th>
+                    <th>Order date</th>
+                    <th>Status</th>
+                    <th>Customer name</th>
+                    <th>Contact</th>
+                    <th>Email</th>
+                    <th>Address</th>
                     <th>Actions</th>
                 </tr>
-                <tr>
-                    <td>1.</td>
-                    <td>Mayank Agarwal</td>
-                    <td>mayank0123</td>
-                    <td>
-                        <a href="#" class=btn-secondary>Update Admin</a>
-                        <a href="#" class=btn-danger>Delete Admin</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>2.</td>
-                    <td>Mayank Agarwal</td>
-                    <td>mayank0123</td>
-                    <td>
-                        <a href="#" class=btn-secondary>Update Admin</a>
-                        <a href="#" class=btn-danger>Delete Admin</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>3.</td>
-                    <td>Mayank Agarwal</td>
-                    <td>mayank0123</td>
-                    <td>
-                        <a href="#" class=btn-secondary>Update Admin</a>
-                        <a href="#" class=btn-danger>Delete Admin</a>
-                    </td>
-                </tr>
+                
+                <?php
+                    //Get all the orders from database
+                    $sql = "SELECT * FROM tbl_order ORDER BY id DESC";
+                    // Execute the query
+                    $res = mysqli_query($conn, $sql);
+                    // Count the rows
+                    $count = mysqli_num_rows($res);
+
+                    $sn = 1; // Create a serial number and set its initial value as 1
+
+                    if ($count>0) {
+                        // Order available
+                        while ($row=mysqli_fetch_assoc($res)) {
+                            // Get all the orders details
+                            $id = $row['id'];
+                            $food = $row['food'];
+                            $price = $row['price'];
+                            $qty = $row['qty'];
+                            $total = $row['total'];
+                            $order_date = $row['order_date'];
+                            $status = $row['status'];
+                            $customer_name = $row['customer_name'];
+                            $customer_contact = $row['customer_contact'];
+                            $customer_email = $row['customer_email'];
+                            $customer_address = $row['customer_address'];
+                            
+                            ?>
+                            <tr>
+                                <td><?php echo $sn++; ?></td>
+                                <td><?php echo $food; ?></td>
+                                <td><?php echo $price; ?></td>
+                                <td><?php echo $qty; ?></td>
+                                <td><?php echo $total; ?></td>
+                                <td><?php echo $order_date; ?></td>
+
+                                <td>
+                                    <?php
+                                        if ($status=="Ordered") {
+                                            echo "<label>$status</label>";
+                                        } elseif ($status=="On Delivery") {
+                                            echo "<label style='color: orange'>$status</label>";
+                                        } elseif ($status=="Delivered") {
+                                            echo "<label style='color: green'>$status</label>";
+                                        } elseif ($status=="Cancelled") {
+                                            echo "<label style='color: red'>$status</label>";
+                                        }
+                                    ?>
+                                </td>
+                                
+                                <td><?php echo $customer_name; ?></td>
+                                <td><?php echo $customer_contact; ?></td>
+                                <td><?php echo $customer_email; ?></td>
+                                <td><?php echo $customer_address; ?></td>
+                                <td>
+                                    <a href="<?php echo SITEURL; ?>admin/update-order.php?id=<?php echo $id; ?>" class=btn-secondary>Update Order</a>
+                                </td>
+                            </tr>
+                            <?php
+                        }
+                    } else {
+                        // Order not available
+                        echo "<tr><td colspan='12' class='error'>Order not available</td></tr>";
+                    }
+                ?>
             </table>
         </div>
     </div>
